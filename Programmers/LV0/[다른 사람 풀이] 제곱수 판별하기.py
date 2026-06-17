7580a4c93b4cb6843e55424ec235fed241d2c586
seojeongyun <tjwjddbs4018@naver.com>
2026-06-17 15:32:37 +0900

    [LV0] 다른 사람 풀이

---
+++ Programmers/LV0/[다른 사람 풀이] 제곱수 판별하기.py
@@ -0,0 +1,22 @@
+'''
+    https://school.programmers.co.kr/learn/courses/30/lessons/120909
+
+    제곱수 판별하기
+'''
+
+import math
+
+def solution(n):
+    return 1 if int(math.sqrt(n)) ** 2 == n else 2
+
+'''
+다른 사람 풀이
+def solution(n):
+    return 1 if (n ** 0.5).is_integer() else 2
+    
+def solution(n):
+    if n**(1/2) == int(n**(1/2)) :
+        return 1
+    else :
+        return 2
+'''
