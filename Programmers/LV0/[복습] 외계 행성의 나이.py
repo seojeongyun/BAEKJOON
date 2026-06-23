fb09720db47c0d1dde5dd7b83c7aab58bfa8e851
seojeongyun <tjwjddbs4018@naver.com>
2026-06-23 18:00:44 +0900

    [LV0] list 뒤집기

--- Programmers/LV0/[복습] 외계 행성의 나이.py
+++ Programmers/LV0/[복습] 외계 행성의 나이.py
@@ -2,6 +2,7 @@
     https://school.programmers.co.kr/learn/courses/30/lessons/120834
     [1] list.reverse()의 return 값은 None이고 list 자체를 뒤집는다.
     [2] reverse() 대신  [::-1] 로 슬라이싱 해도 뒤집힌다.
+    [3] str(int)를 하면 유니코드 반환
 '''
 
 def solution(age):
